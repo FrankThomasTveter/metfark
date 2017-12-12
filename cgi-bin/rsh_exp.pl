b4c726f996e566fc74f8bf294ef36df78c951ace
FrankThomasTveter <f.t.tveter@gmail.com>
2017-12-12 12:14:40 +0100

    Minor bugfixes.

--- cgi-bin/rsh_exp.pl
+++ cgi-bin/rsh_exp.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -w
 #
 use strict;
-use URI::Encode qw(uri_encode);
+#use URI::Encode qw(uri_encode);
 use fark;
 #
 my $scr = "fark_exp.pl";
-my $url = uri_encode($ENV{QUERY_STRING} || join(' ',@ARGV));
+my $url = $ENV{QUERY_STRING} || join(' ',@ARGV);
 my $usr = getpwuid($<);
 #
 fark::remote($scr,$url,$usr);
--- cgi-bin/rsh_load.pl
+++ cgi-bin/rsh_load.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -w
 #
 use strict;
-use URI::Encode qw(uri_encode);
+#use URI::Encode qw(uri_encode);
 use fark;
 #
 my $scr = "fark_load.pl";
-my $url = uri_encode($ENV{QUERY_STRING} || join(' ',@ARGV));
+my $url = $ENV{QUERY_STRING} || join(' ',@ARGV); # uri_encode()
 my $usr = getpwuid($<);
 #
 fark::remote($scr,$url,$usr);
