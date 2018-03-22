ddd7412661ae39bbbd86150d42184a38bb3ec1f1
FrankThomasTveter <f.t.tveter@gmail.com>
2018-03-22 08:32:20 +0000

    Added synop splus-script.

--- clean.sh
+++ clean.sh
@@ -1,7 +1,5 @@
 #!/bin/csh
 #
-cp -pr /var/www/cgi-bin/* cgi-bin/;
-cp -pr /var/www/html/* html/;
 rm -f `find . -name \*.a`;
 rm -f `find . -name \*.so`;
 rm -f `find . -name \*.mod`;
---
+++ copy.sh
@@ -0,0 +1,7 @@
+#!/bin/csh
+#
+cp -pr /var/www/cgi-bin/* cgi-bin/;
+cp -pr /var/www/html/* html/;
+cp -pr /metfark/config/splus/* splus/;
+#
+
